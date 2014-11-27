7583dc0826233142bc13fe2d7a563f1f0a7235a1
unknown <051301397@PC1707.durack.edu.au>
2014-11-27 17:01:26 +0800

    Chanded stuff.

--- www/script.js
+++ www/script.js
@@ -1,10 +1,3 @@
-$( document ).on( "mobileinit", function() {
-  $.mobile.loader.prototype.options.text = "loading";
-  $.mobile.loader.prototype.options.textVisible = true;
-  $.mobile.loader.prototype.options.theme = "a";
-  $.mobile.loader.prototype.options.html = "";
-});
-
 var global =
 {
 	dictionary:
